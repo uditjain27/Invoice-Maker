e6fb48e4c052dc8e6a69ebf4b9e98aac7b8943ad
Uditjain27 <uditjain270699@gmail.com>
2022-01-28 22:06:02 +0530

    Updated Database

--- src/Components/DataBase.js
+++ src/Components/DataBase.js
@@ -1,6 +1,6 @@
 export const data = [
   {
-    id: 10,
+    id: 1,
     name: 'BHATTI HANDLOOM',
     address: 'NEAR BHAIRUDAN JI WELL, SARDARSHAHR, RAJASTHAN - 331403',
     contactNo: 9983025474,
@@ -9,7 +9,7 @@ export const data = [
     gstin: '08AMXPC2156Q1ZB'
   },
   {
-    id: 20,
+    id: 2,
     name: 'PAYAL TEXTILE',
     address : 'P. No. 29, Heera Colony, Ramgarh Road, Jaipur, Rajasthan - 302003',
     contactNo: 'NULL',
@@ -18,7 +18,7 @@ export const data = [
     gstin: '08BQBPK3484J1ZL'
   },
   {
-    id: 30,
+    id: 3,
     name: 'VAIBHAV TEXTILE',
     address : 'PILI KOTHI, GURIABAGH, ALIGARH',
     contactNo: 8445220250,
@@ -27,16 +27,16 @@ export const data = [
     gstin: 'NULL',
     aadhar : 837875902801
   },
-  /* {
-    id: 1,
-    name: ,
-    address,
-    contactNo: ,
-    state:,
-    stateCode: ,
-    gstin:
-  },
   {
+    id: 4,
+    name: 'FATIMA NOOR STORE',
+    address: 'H.No.-115, Gali No. 09, Block-D, Village Wazirabad, North Delhi, Delhi - 110084',
+    contactNo: '9350702270',
+    state: 'DELHI',
+    stateCode: 7,
+    gstin: '07AFEPI0391H1ZN',
+  },
+  /*{
     id: 1,
     name: ,
     address,
